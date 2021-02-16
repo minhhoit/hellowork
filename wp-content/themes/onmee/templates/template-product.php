97f3a7559c36f19213ddd52e1b8863c5cb33cb4a
andrew <hoangminh.it4u@gmail.com>
2021-02-16 19:21:16 +0700

    Commit #7

---
+++ wp-content/themes/onmee/templates/template-product.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Full Width Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Onmee
+ * @since Onmee Onmee 1.0
+ */
+
+get_template_part( 'info' );
